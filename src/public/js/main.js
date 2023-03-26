3f88bcbbaf1acebdd7f6469a934fe5ae8c63cedc
Alvaro Barrero <barreroalvaro2007@gmail.com>
2023-03-26 14:37:16 +0200

    mecanica de ganar arreglada (version final)

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,54 +7,40 @@ const slotsEvents = [];
 for (let i = 0; i < 9; i++) {
   const slot = document.getElementById(i);
   slot.char = "placeholder";
-  slotsEvents.push(slot.addEventListener("click", () => showSlot(slot)));
+  slot.addEventListener("click", () => showSlot(slot));
   slots.push(slot);
 }
 
 reset.addEventListener("click", () => clearGame(slots));
 
 function showSlot(slot) {
-  slot.children[0].style.display = "inline-block";
+  const [imgSlot] = slot.children;
+  imgSlot.style.display = "inline-block";
 
   if (slot.char === "placeholder") {
     if (currentTeam == teams[0]) {
       currentTeam = teams[1];
       slot.char = "x";
-      slot.children[0].setAttribute(
-        "src",
-        "https://www.freeiconspng.com/thumbs/x-png/x-png-33.png"
-      );
+      imgSlot.setAttribute("src", "./imgs/x-img.png");
     } else {
       currentTeam = teams[0];
       slot.char = "o";
-      slot.children[0].setAttribute(
-        "src",
-        "https://upload.wikimedia.org/wikipedia/commons/9/9e/Blue_circle.png"
-      );
+      imgSlot.setAttribute("src", "./imgs/circle-img.png");
     }
   }
 
   switch (currentTeam) {
     case "x":
-      turnSelector.setAttribute(
-        "src",
-        "https://www.freeiconspng.com/thumbs/x-png/x-png-33.png"
-      );
+      turnSelector.setAttribute("src", "./imgs/x-img.png");
       break;
     case "o":
-      turnSelector.setAttribute(
-        "src",
-        "https://upload.wikimedia.org/wikipedia/commons/9/9e/Blue_circle.png"
-      );
+      turnSelector.setAttribute("src", "./imgs/circle-img.png");
       break;
   }
 
-  if (checkWin(slots) != "placeholder") {
-    removeClickEvents(slotsEvents);
-    setTimeout(() => {
-      alert(`Winner: ${checkWin(slots)}`);
-      clearGame(slots);
-    }, 1000);
+  if (checkWin(slots) !== null) {
+    alert(`Winner: ${checkWin(slots)}`);
+    clearGame(slots);
   }
 }
 
@@ -75,19 +61,41 @@ function clearGame(slots) {
 }
 
 function checkWin(slots) {
-  const row1 = slots[0].char == slots[1].char && slots[1].char == slots[2].char;
-  const row2 = slots[3].char == slots[4].char && slots[4].char == slots[5].char;
-  const row3 = slots[6].char == slots[7].char && slots[7].char == slots[8].char;
+  const row1 =
+    slots[0].char != "placeholder" &&
+    slots[0].char == slots[1].char &&
+    slots[1].char == slots[2].char;
+
+  const row2 =
+    slots[3].char != "placeholder" &&
+    slots[3].char == slots[4].char &&
+    slots[4].char == slots[5].char;
+
+  const row3 =
+    slots[6].char != "placeholder" &&
+    slots[6].char == slots[7].char &&
+    slots[7].char == slots[8].char;
+
   const column1 =
-    slots[0].char == slots[3].char && slots[3].char == slots[6].char;
+    slots[0].char != "placeholder" &&
+    slots[0].char == slots[3].char &&
+    slots[3].char == slots[6].char;
   const column2 =
-    slots[1].char == slots[4].char && slots[4].char == slots[7].char;
+    slots[1].char != "placeholder" &&
+    slots[1].char == slots[4].char &&
+    slots[4].char == slots[7].char;
   const column3 =
-    slots[2].char == slots[5].char && slots[5].char == slots[8].char;
+    slots[2].char != "placeholder" &&
+    slots[2].char == slots[5].char &&
+    slots[5].char == slots[8].char;
   const diagonal1 =
-    slots[0].char == slots[4].char && slots[4].char == slots[8].char;
+    slots[0].char != "placeholder" &&
+    slots[0].char == slots[4].char &&
+    slots[4].char == slots[8].char;
   const diagonal2 =
-    slots[2].char == slots[4].char && slots[4].char == slots[6].char;
+    slots[2].char != "placeholder" &&
+    slots[2].char == slots[4].char &&
+    slots[4].char == slots[6].char;
 
   if (row1) return slots[0].char;
   else if (row2) return slots[3].char;
@@ -97,10 +105,5 @@ function checkWin(slots) {
   else if (column3) return slots[2].char;
   else if (diagonal1) return slots[0].char;
   else if (diagonal2) return slots[2].char;
-}
-
-function removeClickEvents(eventList) {
-  for (let event of eventList) {
-    removeEventListener("click", event);
-  }
+  else return null;
 }
